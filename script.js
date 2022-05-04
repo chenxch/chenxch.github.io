e2df0ac8f5ec8186f0194e6112895cb77b254a27
chenxch <124118265@qq.com>
2022-05-04 23:03:57 +0800

    upd

--- script.js
+++ script.js
@@ -146,6 +146,7 @@ function resume() {
 function start() {
   if (!game && Phaser) {
     document.querySelector('#start').style.display = 'none'
+    document.querySelector('body').scrollTo(0,999999)
     game = new Phaser.Game(config)
     // setInterval(()=>{
     //   loading()
